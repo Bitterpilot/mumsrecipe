ed44a58d7b4d9f87f826a3776bd9b88879907661
Nathan Minissale <nathanminissale@gmail.com>
2018-02-15 19:31:18 +0800

    form in working order

--- contactform/views.py
+++ contactform/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .forms import ContactForm
 
-# Create your views here.
+
+# add to your views
+def contact(request):
+    form_class = ContactForm
+
+    return render(request, 'contactform/contact.html', {
+        'form': form_class,
+    })
