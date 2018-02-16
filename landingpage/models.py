5a36b9b1111bed68fd036a0f80ea162df26445d2
Nathan Minissale <nathanminissale@gmail.com>
2018-02-16 12:39:16 +0800

    started admin

--- landingpage/models.py
+++ landingpage/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class LandingPage(models.Model):
+    business_name = models.CharField(_("Business Name"), max_length=200)
+    about = models.TextField(_("About"))
+    main_img = models.ImageField(_("Center piece image"), upload_to="media/upload/landingpage")
